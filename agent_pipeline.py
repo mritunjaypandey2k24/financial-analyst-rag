5166a9d64bed32226a289b11ff02df6918a67406
mritunjaypandey2k24 <mritunjaypandey.ee@gmail.com>
2026-01-14 12:33:34 +0530

    Refactor answer_query method for clarity and structure

--- agent_pipeline.py
+++ agent_pipeline.py
@@ -2,6 +2,7 @@
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from utils.retriever import DocumentRetriever
+from typing import List
 
 class FinancialRAGAgent:
     def __init__(self, llm_model_name: str = 'tiiuae/falcon-7b-instruct'):
@@ -11,19 +12,21 @@ class FinancialRAGAgent:
         self.model = AutoModelForCausalLM.from_pretrained(llm_model_name, device_map='auto')
 
     def answer_query(self, query: str, documents: List[str]) -> str:
-    """Retrieve context and generate an answer."""
-    embeddings = self.retriever.create_embeddings(documents)
-    self.retriever.build_index(embeddings)
-    
-    context_indices = self.retriever.query(query)
-    relevant_context = "".join([documents[i] for i in context_indices])
-    
-    # Truncate the context to fit the model's max length
-    max_context_length = 2000  # Reasonable buffer below 2048
-    truncated_context = relevant_context[:max_context_length]
+        """Retrieve context and generate an answer."""
+        embeddings = self.retriever.create_embeddings(documents)
+        self.retriever.build_index(embeddings)
+        
+        context_indices = self.retriever.query(query)
+        relevant_context = "".join([documents[i] for i in context_indices])
+        
+        # Truncate input to fit model's max input length
+        max_context_length = 2000
+        truncated_context = relevant_context[:max_context_length]
 
-    inputs = self.tokenizer(
-        f'{truncated_context} Question: {query}', return_tensors='pt'
-    ).to('cuda' if torch.cuda.is_available() else 'cpu')
-    outputs = self.model.generate(inputs['input_ids'], max_length=512)
-    return self.tokenizer.decode(outputs[0])
+        # Tokenize the input and send it to appropriate device (CUDA or CPU)
+        inputs = self.tokenizer(
+            f'{truncated_context} Question: {query}',
+            return_tensors='pt'
+        ).to('cuda' if torch.cuda.is_available() else 'cpu')
+        outputs = self.model.generate(inputs['input_ids'], max_length=512)
+        return self.tokenizer.decode(outputs[0])
